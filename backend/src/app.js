d6b67054d5f69191da61130b6d343a73b7684cbb
mikmak29 <mjogamaapple@gmail.com>
2026-01-08 17:33:14 +0800

    Hotfix/secure-endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
-import cookies from "cookie-parser";
+import cookieParser from "cookie-parser";
 import express from "express";
 
 import userRoutes from "./routes/user.route.js";
@@ -9,14 +9,21 @@ import userRoutes from "./routes/user.route.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
-app.use(cookies());
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL || "http://127.0.0.1:5500", // Your frontend URL (domain only, not full path)
+		credentials: true, // Allow cookies
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"], // Allow Authorization header
+	})
+);
+app.use(cookieParser());
 app.use(express.json());
 app.use(compression());
 
 app.use("/api/user", userRoutes);
 app.use("/health", (req, res) => {
-	res.status(200).send("Server is working", process.uptime);
+	res.status(200).json("Server is working", process.uptime);
 });
 
 export default app;
